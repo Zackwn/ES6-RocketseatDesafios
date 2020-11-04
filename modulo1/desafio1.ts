ad1f6564ef415e4494f53e87c646e35cd6149e68
Zackwn <rafaelfavazzi@gmail.com>
2020-11-04 16:46:01 -0300

    modulo 1. desafio 1

---
+++ modulo1/desafio1.ts
@@ -0,0 +1,28 @@
+class User {
+  public password: string
+  public email: string
+  protected admin: boolean
+
+  public constructor(email: string, password: string) {
+    this.email = email
+    this.password = password
+    this.admin = false
+  }
+
+  public get isAdmin(): boolean {
+    return this.admin
+  }
+}
+
+class Admin extends User {
+  public constructor(email: string, password: string) {
+    super(email, password)
+    this.admin = true
+  }
+}
+
+const user = new User('example@example.com', '123456')
+console.log(user.isAdmin) // false
+
+const admin = new Admin('example@example.com', '123456')
+console.log(admin.isAdmin) // true
